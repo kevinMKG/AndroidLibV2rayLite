b52a8b158c7a4de0b98db9991290bc8380d9f83d
Piter Hans <85574687+kevinMKG@users.noreply.github.com>
2021-06-24 16:39:23 +0800

    Update v2logger.go

--- v2logger.go
+++ v2logger.go
@@ -4,7 +4,7 @@ import (
 	"log"
 	"os"
 
-	v2commlog "github.com/kevinMKG/vtsttt/v4/common/log"
+	v2commlog "github.com/kevinMKG/v2ray-core/v4/common/log"
 )
 
 // This struct creates our own log writer without datatime stamp
